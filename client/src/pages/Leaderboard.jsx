d4fba7d78d1226d916529a45dd0e07d097620e04
vaishnavi shridhar <vaishnavishridhar23@gmail.com>
2024-03-26 22:15:57 +0530

    Leaderboard modified

--- client/src/pages/Leaderboard.jsx
+++ client/src/pages/Leaderboard.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
+import "./leaderboard.css";
 
 function Leaderboard() {
   const [topWalletScoreUsers, setTopWalletScoreUsers] = useState([]);
@@ -25,7 +27,7 @@ function Leaderboard() {
           setTopWalletScoreUsers(walletScoreData);
         })
         .catch((error) => {
-          console.error(error);
+          setError(error.message);
         });
     };
 
@@ -49,37 +51,64 @@ function Leaderboard() {
         .catch((error) => {
           setError(error.message);
         });
-    }
+    };
 
     fetchTopPerformers();
     fetchTopPortfolios();
   }, []);
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   return (
-    <div>
-      <h2>Top 5 Wallet Score Performers</h2>
-      <ul>
-        {topWalletScoreUsers.map((user, index) => (
-          <li key={index}>
-            {user.userID} - Wallet Score: {user.walletScore}
-          </li>
+    <div className="container">
+      <div className="graph-container">
+        <h2 className="graph-title">Top 5 Wallet Score Performers</h2>
+        {topWalletScoreUsers.map((performer, index) => (
+          <div key={index}>
+            <h3 className="values">{index + 1}. {performer.userID}</h3>
+            <div className="graphs">
+            <Plot
+              data={[
+                {
+                  x: ['Web3 Reputation', 'Authenticity'],
+                  y: [performer.score.web3ReputationScore, performer.score.authenticityScore],
+                  type: "bar",
+                  marker: { color: ["blue", "green"] },
+                },
+              ]}
+              layout={{
+                width: 400,
+                height: 300,
+                title: "Wallet Score Distribution",
+                paper_bgcolor: 'rgba(137, 113, 208, 0.1)',
+                plot_bgcolor: 'rgba(137, 113, 208, 0.1)',
+                font: { color: "white" }
+              }}
+            />
+            <Plot
+              data={[
+                {
+                  x: ['Portfolio Value'],
+                  y: [performer.totalScore],
+                  type: "bar",
+                  marker: { color: "orange" },
+                },
+              ]}
+              layout={{
+                width: 400,
+                height: 300,
+                title: "Portfolio Value",
+                paper_bgcolor: 'rgba(137, 113, 208, 0.1)',
+                plot_bgcolor: 'rgba(137, 113, 208, 0.1)',
+                font: { color: "white" }
+              }}
+            />
+            </div>
+          </div>
         ))}
-      </ul>
-
-      <h2>Top 5 Portfolio Values Performers</h2>
-      <ul>
-        {topPortfolioValuesUsers.map((user, index) => (
-          <li key={index}>
-            {user.name} - Portfolio Value: {user.portfolioValue}
-          </li>
-        ))}
-      </ul>
+        {error && <p>{error}</p>}
+      </div>
     </div>
   );
+  
 }
 
 export default Leaderboard;
